575536f6c6d1b82dd67e48553426efb978b00ef7
Logstor <a.rottger_rydahl@live.dk>
2022-07-26 10:42:34 +0200

    Added AuthModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,25 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AuthComponent } from './auth/auth/auth.component';
 import { RecipesModule } from './recipe/recipes.module';
 import { ShoppingModule } from './shopping/shopping.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    AuthComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
     ShoppingModule,
+    AuthModule,
     CoreModule,
     SharedModule
   ],
