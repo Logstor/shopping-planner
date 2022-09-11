eafcc90525a65d87c1337a2467324c35011ab5d4
Logstor <a.rottger_rydahl@live.dk>
2022-09-11 18:55:17 +0200

    Fixed storeShoppingList effect

--- src/app/shopping/store/shopping-list.effects.ts
+++ src/app/shopping/store/shopping-list.effects.ts
@@ -2,11 +2,10 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Actions, concatLatestFrom, createEffect, ofType } from "@ngrx/effects";
 import { Store } from "@ngrx/store";
-import { map, switchMap, tap } from "rxjs/operators";
+import { map, switchMap, take, tap } from "rxjs/operators";
 import { Ingredient } from "src/app/shared/Ingredient";
 import * as ShoppingListActions from "src/app/shopping/store/shopping-list.actions";
 import { AppState } from "src/app/store/app.reducer";
-import { ShoppingState } from "./shopping-list.reducer";
 
 @Injectable({
     providedIn: 'root'
@@ -19,19 +18,19 @@ export class ShoppingListEffects
         private readonly http: HttpClient
     ) {}
 
-    storeShoppingList = createEffect(() => {
+    storeShoppingList$ = createEffect(() => {
         return this.actions$
             .pipe(
                 ofType(ShoppingListActions.STORE_INGREDIENTS),
-                switchMap(() => this.store.select('shoppingList')),
-                switchMap((shoppingListState: ShoppingState) => this.http.put(
+                concatLatestFrom(() => this.store.select('shoppingList')),
+                switchMap(([action, shoppingListState]) => this.http.put(
                     "https://shoppingplanner-8923c-default-rtdb.europe-west1.firebasedatabase.app/shopping-list.json",
                     shoppingListState.ingredients
                 ))
             )
     }, { dispatch: false });
 
-    fetchShoppingList = createEffect(() => {
+    fetchShoppingList$ = createEffect(() => {
         return this.actions$
             .pipe(
                 ofType(ShoppingListActions.FETCH_INGREDIENTS),
