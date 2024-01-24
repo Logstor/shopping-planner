3a3a63342431af14dbfddcbe773f0b841f0afaf4
Logstor <a.rottger_rydahl@live.dk>
2024-01-24 21:33:54 +0100

    ShoppingListComponent making use of new Actions and Selectors

--- src/app/shopping/shopping-list/shopping-list.component.ts
+++ src/app/shopping/shopping-list/shopping-list.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { combineLatest } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
-import * as SLA from '../store/shopping-list.actions';
 import { shoppingListSelectors } from '../store/shopping-list.selectors';
+import { shoppingListActions } from '../store/shopping-list.actions';
 
 @Component({
   selector: 'app-shopping-list',
@@ -11,9 +13,54 @@ import { shoppingListSelectors } from '../store/shopping-list.selectors';
 })
 export class ShoppingListComponent implements OnInit, OnDestroy
 {
-  public readonly ingredients$ = this.store.select(
-    shoppingListSelectors.selectIngredientsSorted
+  @Input('shoppingListId')
+  public listId: string;
+
+  @Input('isSharedShoppingList')
+  public isShared: boolean;
+
+  public readonly ingredients$ = combineLatest([
+    this.store.select(shoppingListSelectors.selectShoppingLists),
+    this.store.select(shoppingListSelectors.selectSharedShoppingLists)
+  ]).pipe(
+    map(([shoppingLists, sharedShoppingLists]) => {
+      //TODO: Update this part to make use of the isShared flag
+      const shoppingList = shoppingLists.find(list => list.id === this.listId);
+      if (shoppingList)
+        return shoppingList;
+      else
+      {
+        const sharedShoppingList = sharedShoppingLists.find(list => list.id === this.listId);
+        if (sharedShoppingList)
+          return sharedShoppingList;
+        else
+          return null;
+      }
+    }),
+    filter(shoppingList => {
+      // Guard against null
+      if (!shoppingList) return false;
+
+      // If it's the first call
+      if (!this.currentStringify)
+      {
+        this.currentStringify = JSON.stringify(shoppingList);
+        return true
+      }
+      
+      // If the list has changed
+      if (this.currentStringify !== JSON.stringify(shoppingList))
+      {
+        this.currentStringify = JSON.stringify(shoppingList);
+        return true
+      }
+
+      // If we reach this point, the list has not changed
+      return false;
+    })
   );
+
+  private currentStringify?: string = null
 
   constructor(
     private readonly store: Store
@@ -25,12 +72,20 @@ export class ShoppingListComponent implements OnInit, OnDestroy
 
   onEditItem(index: number): void
   {
-    this.store.dispatch(new SLA.StartEdit(index));
+    this.store.dispatch(
+      shoppingListActions.startEdit({
+        ingredientIndex: index,
+        listId: this.listId,
+        isShared: this.isShared
+      })
+    );
   }
 
-  onRemoveItem(index: number): void
+  onRemoveItem(ingredientName: string): void
   {
-    this.store.dispatch(new SLA.StartEdit(index));
-    this.store.dispatch(new SLA.DeleteIngredient());
+    this.store.dispatch(shoppingListActions.componentRemoveIngredient({
+      listId: this.listId,
+      ingredientName: ingredientName
+    }))
   }
 }
